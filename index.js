c3856c0d107b34ae5f4a49ab1d3209826fbd9804
Franco <francomazns@gmail.com>
2024-08-28 16:26:57 -0500

    feat(login/register): Se subio la logica del login y register

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 import express from 'express'
 import {PORT} from './config.js'
 import { UserRepository } from './user-repository.js'
+
 const app = express()
-app.use(express.json())
+app.set('view engine', 'ejs')
+app.use(express.json());
 
 app.get('/',(req, res)=>{
-    res.send('Hello Franco xd')
-})
+    res.render('example', {username : 'Franquito'})
+});
 
-app.post('/login',(req, res)=>{})
+//LOGIN AUTH
+app.post('/login', async (req, res)=>{
+    const { username , password } = req.body;
+    console.log("req.body: ", req.body);
+    try {
+        const user = await UserRepository.login({username,password})
+        res.send({user})
+    } catch (error) {
+        res.status(401).send(error.message);
+    }
+});
+
+//REGISTER AUTH
 app.post('/register', async (req, res)=>{
-    const { username , password }= req.body //???
+    const { username , password } = req.body //???
     console.log("req.body: ", req.body);
     
     try {
         const id = await UserRepository.create({username,password})
         res.send({id})
     } catch (error) {
-        console.log("error:", error);
         res.status(400).send(error.message)
     }
+});
 
-})
 app.post('/logout',(req, res)=>{})
 app.post('/protected',(req, res)=>{})
 
--- user-repository.js
+++ user-repository.js
@@ -1,30 +1,27 @@
-import crypto from 'crypto';
+import crypto from 'node:crypto';
 import bcrypt from 'bcrypt';
 import DBLocal from "db-local";
 
 import { SALT_ROUNDS } from './config.js';
-const {Schema} = new DBLocal({path: './db'})
+const { Schema } = new DBLocal({ path: './db' })
 
-const User = Schema('User',{
-    _id: {type: String, required: true}, 
-    _username: {type: String, required: true},
-    _password: {type: String, required: true}
+const User = Schema('User', {
+    _id: { type: String, required: true },
+    _username: { type: String, required: true },
+    _password: { type: String, required: true }
 })
 
-export class UserRepository{
-    static async create ({username, password}) {
+export class UserRepository {
+    static async create({ username, password }) {
         // Validaciones de username(opcional: usar Zod)
-        if(typeof username !== 'string') throw new Error("Username must be a string");
-        if(username < 3) throw new Error("Username must be at least 3 characters long");
-
-        if(typeof password != 'string') throw new Error("Password must be a string");
-        if(password < 6) throw new Error("Password must be at least 6 characters long");
+        Validation.username(username);
+        Validation.password(password);
 
         // Asegurarse que el username no existe
-        const user = User.findOne({_username: username})
+        const user = User.findOne({ _username: username })
         if (user) throw new Error("Username already exist");
         const id = crypto.randomUUID();
-        const hashedPassword = await bcrypt.hashSync(password,SALT_ROUNDS);
+        const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
 
         User.create({
             _id: id,
@@ -33,7 +30,41 @@ export class UserRepository{
         }).save()
 
         return id
-        
+    };
+
+    static async login({ username, password }) {        
+        Validation.username(username);
+        Validation.password(password);
+
+        const user = User.findOne({_username: username})
+        if(!user) throw new Error("username doesnt exist");
+
+        console.log("Stored hashed password:", user._password)
+
+        const isValid = await bcrypt.compare(password, user._password)
+        if(!isValid) throw new Error("password is invalid")
+
+        // -- Solo los que no quieres devolver --
+        // const { _password: _, ... publicUser } = user
+        // return publicUser
+
+        return {
+            id: user._id,
+            username: user._username,
+        } // BEST WAY (Los que quieres devolver)
+    };
+
+}
+
+// VALIDATION USERNAME && PASSWORD
+class Validation {
+    static username(username){
+        if (typeof username !== 'string') throw new Error("Username must be a string");
+        if (username.length < 3) throw new Error("Username must be at least 3 characters long");
     }
-    static login ({username, password}) {}
+
+    static password(password){
+        if (typeof password != 'string') throw new Error("Password must be a string");
+        if (password.length < 6) throw new Error("Password must be at least 6 characters long");
+    }
 }
